f3a7db46bb0cbea114fa0b824646ad39d1e5eeed
Marcus Reynolds <marcusroy93@gmail.com>
2021-07-17 09:56:19 -0700

    added uvi HTML elements

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
 
 const search = document.getElementById('searchButton');
-
 search.addEventListener('click', searchBarFunction);
 
 function searchBarFunction(event) {
     event.preventDefault();
     let input = document.getElementById('cityUserInput').value;
-    console.log(input);
+    localStorage.setItem('previousCitySearch',`${document.getElementById('cityUserInput')}`)
     findCityLatAndLon(input);
 };
-
+//fetches the coordinates for a city input to be put into the one call api
 function findCityLatAndLon(city){
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appId=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`).then(response => response.json()).then(weatherData => {
         displayForecast(weatherData.coord);
@@ -18,47 +17,49 @@ function findCityLatAndLon(city){
         console.log(coordinates);
     })
 };
-
+//displays date for forecast
 function displayDate () {
     let date = Date().split(' ');
     var monthDay = [date[0], date[1], date[2]];
     return monthDay
 };
-
+//for this I created 10 rgb points going from green to red to indiciate UVI intensity
 function uvIndex(uviNumber) {
     switch (uviNumber) {
         case 0:
-            return rgb(0, 255, 0);
+            return 'rgb(0, 255, 0)';
         case 1: 
-            return rgb(51, 255, 0);
+            return 'rgb(51, 255, 0)';
         case 2:
-            return rgb(102, 255, 0);;
+            return 'rgb(102, 255, 0)';
         case 3:
-            return rgb(153, 255, 0);;
+            return 'rgb(153, 255, 0)';
         case 4:
-            return rgb(204, 255, 0);;
+            return 'rgb(204, 255, 0)';
         case 5:
-            return rgb(255, 255, 0);
+            return 'rgb(255, 255, 0)';
         case 6:
-            return rgb(255, 204, 0);
+            return 'rgb(255, 204, 0)';
         case 7:
-            return rgb(255, 153, 0);
+            return 'rgb(255, 153, 0)';
         case 8:
-            return rgb(255, 102, 0);
+            return 'rgb(255, 102, 0)';
         case 9:
-            return rgb(255, 51, 0);
+            return 'rgb(255, 51, 0)';
         case 10:
-            return rgb(255, 0, 0);
+            return 'rgb(255, 0, 0)';
     }
 };
-
+//inputs coordinates for one call api, calls it, then appends current day weather, then loops through 5 day forecast.
 function displayForecast(coords){
     const lat = coords.lat;
     const lon = coords.lon;
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`)
         .then(response => response.json())
         .then(weatherData => { 
-            document.getElementById('currentUvi').style.backgroundColor = uvIndex
+            document.getElementById('currentCity').innerHTML = `Current Weather: ${document.getElementById('cityUserInput').value}`;
+            document.getElementById('currentUviBackground').style.backgroundColor = uvIndex(parseInt(weatherData.current.uvi));
+            document.getElementById('currentUvi').innerHTML = `UVI: ${weatherData.current.uvi}`;
             document.getElementById('day').innerHTML = displayDate()[0];
             document.getElementById('currentDate').innerHTML =  `${displayDate()[1]} ${parseInt(displayDate()[2])}`
             console.log(weatherData.current.weather[0].description);
@@ -69,6 +70,8 @@ function displayForecast(coords){
             document.getElementById('currentHumidity').innerHTML = `Humidity: ${weatherData.current.humidity}%`;
             for (let i = 0; i < 5; i++) {
                 console.log(weatherData);
+                document.getElementsByClassName('uvi')[i].style.backgroundColor = uvIndex(parseInt(weatherData.daily[i].uvi));
+                document.getElementsByClassName('uvi')[i].innerHTML = `UVI: ${weatherData.daily[i].uvi}`;
                 document.getElementsByClassName('weatherIcon')[i].src = `http://openweathermap.org/img/wn/${weatherData.daily[i].weather[0].icon}@2x.png`;
                 document.getElementsByClassName('weather')[i].innerHTML = weatherData.daily[i].weather[0].description;
                 document.getElementsByClassName('date')[i].innerHTML = `${displayDate()[1]}, ${parseInt(displayDate()[2]) + (1 + i) }`;
@@ -77,8 +80,6 @@ function displayForecast(coords){
                 document.getElementsByClassName('humidity')[i].innerHTML = `Humidity: ${weatherData.daily[i].humidity}%`;
                 document.getElementsByClassName('windSpeed')[i].innerHTML = `Wind: ${weatherData.daily[i].wind_speed} mph`;
             }
-            console.log(weatherData);
-            let selectDay = document.querySelector('#forecastDay1');
         });
 };
 
